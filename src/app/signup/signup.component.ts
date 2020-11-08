362a6e7cd1393585df9e8325dba1a17ccc13f16c
AbyssalMager <zhang.mark1@gmail.com>
2020-11-08 01:28:55 -0800

    Added something to let you know when you clicked and a buggy version of form submission

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
+  name: string;
+  email: string;
+  password: string;
+
   constructor() {}
 
   ngOnInit(): void {}
+  /**
+   * Process the form we have. Send to whatever backend
+   * Only alerting for now
+   */
+  processForm() {
+    const allInfo = `My name is ${this.name}. My email is ${
+      this.email
+    }. My message is ${this.password}`;
+    alert(allInfo);
+  }
 }
