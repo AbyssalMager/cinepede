5f6dd6c7d81fd3f064c622d31f65f8a91c8a8c6e
AbyssalMager <zhang.mark1@gmail.com>
2020-11-08 23:24:03 -0800

    the website works now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
@@ -7,7 +8,7 @@ import { SignupComponent } from "./signup/signup.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, SignupComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
