a84de649c7cc81f0a33a1aef5ccb5d87664d6410
AbyssalMager <zhang.mark1@gmail.com>
2020-11-12 00:19:48 -0800

    Routing complete, signup form finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component"
 import { HomeComponent } from "./home/home.component";
 import { SignupComponent } from "./signup/signup.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SignupComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent, 
+    HeaderComponent, 
+    HomeComponent, 
+    SignupComponent
+    ],
+  imports: [
+    BrowserModule,
+     FormsModule,
+     AppRoutingModule
+     ],
   providers: [],
   bootstrap: [AppComponent]
 })
